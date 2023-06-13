b3de452b675e993f25787c9b7cc5eb4a1e93e1c6
Eugene Bernardo <beugenecarlo@gmail.com>
2023-06-13 19:43:54 +0200

    Add project files.

---
+++ Baalhyttebooking/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Baalhyttebooking
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
